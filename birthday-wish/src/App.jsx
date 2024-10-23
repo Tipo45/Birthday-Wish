d582302c47a604fabde6c5d4720bd9da4b0d1586
Azebi-Alex Steven .T. <tipo4542@gmail.com>
2024-10-23 23:19:31 +0100

    second commit

--- birthday-wish/src/App.jsx
+++ birthday-wish/src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Confetti from "./Confetti";
 import Countdown from "./Countdown";
 
 function App() {
   return (
     <>
       <div><Countdown /></div>
-      <div><Confetti /></div>
     </>
   );
 }
--- birthday-wish/src/Countdown.jsx
+++ birthday-wish/src/Countdown.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 import "../src/Countdown.css";
 import CakeIcon from '@mui/icons-material/Cake';
+import ReactConfetti from "react-confetti";
 
 const Countdown = () => {
 
@@ -8,11 +9,14 @@ const Countdown = () => {
     const [timerhours, setTimerHours] = useState('00');
     const [timerMinutes, setTimerMinutes] = useState('00');
     const [timerSeconds, setTimerSeconds] = useState('00');
+    const [isButtonActive, setIsButtonActive] = useState(false);
+    const [showPopup, setShowPopup] = useState(false)
+    
 
     let interval = useRef();
 
     const startTimer = () => {
-        const countDownDate = new Date('October 29, 2024 00:00:00').getTime();
+        const countDownDate = new Date('October 23, 2024 00:00:00').getTime();
 
         interval - setInterval(() => {
             const now = new Date().getTime();
@@ -24,7 +28,8 @@ const Countdown = () => {
             const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
             if (distance < 0) {
-                clearInterval(interval.current)
+                clearInterval(interval.current);
+                setIsButtonActive(true);
             } else {
                 setTimerDays(days);
                 setTimerHours(hours);
@@ -41,6 +46,36 @@ const Countdown = () => {
         }
     })
 
+    const [windowDimension, setWindowDimension] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    
+      const [btn, setBtn] = useState(false);
+    
+      const detectSize = () => {
+        setWindowDimension({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+    
+      useEffect(() => {
+        window.addEventListener("resize", detectSize);
+        return () => {
+          window.removeEventListener("resize", detectSize);
+        };
+      }, [windowDimension]);
+
+      const handleButtonClick = () => {
+        setShowPopup(true);
+        setBtn(true)
+    };
+
+    const closePopup = () => {
+        setShowPopup(false); 
+    };
+
   return (
   <>
   <section className="timer-container">
@@ -48,7 +83,7 @@ const Countdown = () => {
         <div>
             <span><CakeIcon className="timer-icon" /></span>
             <h2>Countdown Timer</h2>
-            <p>Countdown to a really special day my love</p>
+            <p>Countdown to a really special.</p>
         </div>
         <div>
             <section>
@@ -76,6 +111,31 @@ const Countdown = () => {
         </div>
     </section>
   </section>
+  <div>
+        <button disabled={!isButtonActive} onClick={handleButtonClick} className="btn">
+          {isButtonActive ? "Click Me!!" : "Counting Down..."}
+        </button>
+        {btn && (
+          <ReactConfetti
+            width={windowDimension.width}
+            height={windowDimension.height}
+            tweenDuration={1000}
+          />
+        )}
+      </div>
+
+      {showPopup && (
+                <div className="popup">
+                    <div className="popup-content">
+                        <h3>Happy Birthday Dear!</h3>
+                        <h5>The Special day has arrived!!</h5>
+                        <p>On your special day, I just want to remind you how amazing you are. You`re not just my sister, but my best friend, confidant, and biggest supporter. I`m so lucky to have you by my side. </p>
+                        <p>As you celebrate today, I hope your heart is filled with as much joy as you bring to everyone around you. May this year bring you endless happiness, success, and all the love you deserve.</p>
+                        <p>Enjoy every moment – today is all about you! Love you always! ❤️🎂</p>
+                        <button onClick={closePopup} className="btns">Close</button>
+                    </div>
+                </div>
+            )}
   </>
   );
 };
